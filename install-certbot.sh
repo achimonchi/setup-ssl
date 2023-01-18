36d80501c1346fafa37f38c4478ff2082552799c
Jovie <reyhanjovie01@gmail.com>
2023-01-18 15:21:35 +0700

    Create install-certbot.sh

---
+++ install-certbot.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+# uncomment this if u need
+#sudo add-apt-repository ppa:certbot/certbot
+#sudo apt-get update
+#sudo apt-get install python-certbot-nginx
+sudo apt install certbot python3-certbot-nginx
